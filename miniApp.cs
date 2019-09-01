eb862bd9dd7b27d32ba911b39badc70e9a88789a
davideastmond <davebuck1981@gmail.com>
2019-08-31 20:16:38 -0400

    commenting and documentation

--- miniApp.cs
+++ miniApp.cs
@@ -27,12 +27,21 @@ namespace SearchFlights
 
     public void Start() 
     {
+      // This is a 'mini-app' that allows scrolling through a list of airports and codes as fetched by the API.
+      // Use the up and down arrow to scroll. Any other key will abort the app and force airport code to be entered.
       int iValue = 0;
+
+      // Show the first page of the airport destinations and codes as soon as the mini-app is initialized.
+      // Then start a loop that accepts up, down or other input to scroll up, down or abort the interface.
       displayAirportPage(iValue);
       while (true)
       {
         ConsoleKeyInfo keyInfo = Console.ReadKey();
         
+        // When the up or down keyboard input is accepted, we'll bump up or down the iValue - basically our pages, allowing us to scroll
+        // through the pages of codes and destinations, back and forth. We call a function that displays the items based on the page. If there was
+        // an error displaying the items, then we don't refresh the page with new items, otherwise, we bump up or down the iValue allowing a page
+        // change.
         if (keyInfo.Key == ConsoleKey.DownArrow) {
           iValue++;
           if (displayAirportPage(iValue)) {
@@ -59,8 +68,6 @@ namespace SearchFlights
     }
 
     private bool displayAirportPage(int index) {
-
-      
       int startPos = index * maxItems;
       int endPos = index * maxItems + maxItems;
       
